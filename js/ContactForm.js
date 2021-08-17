bfe6d0c1f75bb5ddba46470e05b3b4b3545bacfa
SanketTambe39 <sankettambe39@gmail.com>
2021-08-18 00:59:17 +0530

    ADDED: Code for adding data to local store

--- js/ContactForm.js
+++ js/ContactForm.js
@@ -49,8 +49,28 @@ window.addEventListener("DOMContentLoaded", (event) => {
 });
 
 function save() {
-    let contact = new Contact()
-    contact.id = new Date().getTime()
+    try {
+        let contact = createContact();
+        craeteAndUpdateStorage(contact)
+    } catch (error) {
+        alert(error);
+    }
+}
+
+function craeteAndUpdateStorage(contact) {
+    let contactList = JSON.parse(localStorage.getItem("ContactList"))
+    if (contactList != undefined) {
+        contactList.push(contact)
+    } else {
+        contactList = [contact]
+    }
+    alert("Contact Added Sucessfully")
+    localStorage.setItem("contactList", JSON.stringify(contactList))
+}
+
+function createContact() {
+    let contact = new Contact();
+    contact.id = new Date().getTime();
     try {
         contact.name = getInputValueById("#name");
     } catch (error) {
@@ -85,7 +105,7 @@ function save() {
         throw error;
     }
 
-    alert(contact.toString());
+    return contact;
 }
 
 function getInputValueById(property) {
